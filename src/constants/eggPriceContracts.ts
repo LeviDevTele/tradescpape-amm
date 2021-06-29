a0999598db5b88f328e0930effd4d6da6954377e
LeviXDev21 <86292578+LeviXDev21@users.noreply.github.com>
2021-06-29 20:54:02 +0530

    Update eggPriceContracts.ts

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
+  cakeAddress: '0x8370f39682170550f56ae8b17e953fe070bcf235',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
+  lpAddress: '0x5fE1FA34Fc17E845Ec5eC03c1bC8212Be05E8796'
 }
 
 export default priceContracts
